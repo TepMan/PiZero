22a466646da22043d37d3a40d09ce471bbf7b17f
Andreas Tepper <tepper.andreas@gmail.com>
2021-07-29 20:27:17 +0200

    bme 1st try

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+import bme
 import time
 from display_control import display_clear, display_status
 
-display_clear()
-control = True
+# display_clear()
+# control = True
+#
+# while control:
+#     for i in range(100):
+#         display_status(i)
+#         time.sleep(0.5)
+#
+#     display_clear()
+#
+#     char = input("Zum Beenden x drücken: ")
+#
+#     if char == 'x' or char == 'X':
+#         control = False
 
-while control:
-    for i in range(100):
-        display_status(i)
-        time.sleep(0.5)
+print("BME Test:")
 
-    display_clear()
-
-    char = input("Zum Beenden x drücken: ")
-
-    if char == 'x' or char == 'X':
-        control = False
+bme.init("/dev/i2c-1", 119)
+bme.set_timezone("TZ=Europe/London")   # other options include "TZ=Asia/Singapore" or "TZ=US/Denver"
+bme.forced_mode()
+print(" year mon day h min sec  Tmp degC            Prs Pa         Hum %rH           GsR Ohm   Status")
+data = bme.get_data()
+print(data)
+print("Installation succeeded")
