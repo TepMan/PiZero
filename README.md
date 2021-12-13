2f06d4dd2a9ce5682fa3a663faffc3686c7b0a98
TepMan <tepper.andreas@gmail.com>
2021-12-13 19:07:35 +0100

    Projekt leer gemacht

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # PiZero
-PiZero Air Quality Sensor with BME688 and OLED Display.
-
-Measures air quality index via Bosch BME688 and displays quality with 7 blocks on an OLED display (1 block - worst air quality, 7 blocks - excellent air quality).
-
-Used hardware:
-- Pi Zero WH
-- BME688 breakout board from Pi3g
-- Waveshare OLED Display (monochrome, 128x32)
-
-![Hardware](https://github.com/TepMan/PiZero/blob/master/IMG_20210731_202917.jpg "Hardware")
+PiZero Air Quality Sensor 
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import time
-from bme68x_control import read_data
-from display_control import display_clear, display_blocks
 from mqtt_comm import send_data
-
-display_clear()
 
 print("Air Qualitiy Measurement Tool v1.0")
 print("==================================")
@@ -11,22 +7,7 @@ print(" ")
 
 try:
     while True:
-        data = read_data()
-
-        display_blocks(int(data["iaq-state"]))
-
-        print("Timestamp   : " + data["timestamp"])
-        print("Air Quality : " + str(data["iaq"]))
-        # print("Description : " + data["description"])
-        # print("IAQ State   : " + data["iaq-state"].name)
-        print("Temperature : " + '{:.1f}°C'.format(data["temperature"]))
-        print("Humidity    : " + '{:.1f}%'.format(data["humidity"]))
-        print(" ")
-
-        send_data(data)
-
         time.sleep(30)
 
 except KeyboardInterrupt:
-    display_clear()
     print("Program interrupted by user!")
