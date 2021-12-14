1c4976dce285bf8eb94401bf21d46bdc69b0c64b
TepMan <tepper.andreas@gmail.com>
2021-12-14 17:25:24 +0100

    aq-Lib eingebunden

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import time
-from mqtt_comm import send_data
+
+from lib.aq import AQ
 
 print("Air Qualitiy Measurement Tool v1.0")
 print("==================================")
 print(" ")
 
+aq = AQ()
+
 try:
     while True:
-        time.sleep(30)
+        coValue = aq.get_eco2()
+        tmpValue = aq.get_temp()
+        print("CO2-Wert ist " + str(coValue) + " Temp. ist " + str(tmpValue))
+        time.sleep(15)
 
 except KeyboardInterrupt:
     print("Program interrupted by user!")
